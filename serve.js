1697de89d4452e9742d6385a4b1507553d841956
Ev Bogue <ev@evbogue.com>
2023-10-22 09:48:08 -0500

    does this fix it

--- serve.js
+++ serve.js
@@ -17,10 +17,10 @@ const kv = await Deno.openKv()
 const key = ["counter"]
 
 channel.onmessage = async e => {
-  const current = await kv.get(key)
-  const next = (!current.value) ? 1 : ++current.value
-  await kv.set(key, next)
   (e.target != channel) && channel.postMessage(e.data)
+    const current = await kv.get(key)
+    const next = (!current.value) ? 1 : ++current.value
+    await kv.set(key, next)
     channel.postMessage(next)
     sockets.forEach(s => s.send(next))
 }
