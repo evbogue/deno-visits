4f6e3acc8bd86c2c2dab4e745af2ac08523b5471
Ev Bogue <ev@evbogue.com>
2023-10-22 09:23:39 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Visits number
+
+Show how many visits we've had in realtime using Deno.broadcastChannel and Deno.Kv
+
+```
+deno run --allow-all --unstable serve.js
+```
---
+++ serve.js
@@ -0,0 +1,46 @@
+const html = `
+  <script>
+    const ws = new WebSocket((window.location.protocol === 'https:' ? 'wss://' : 'ws://') + location.host)
+    ws.onopen = e => {ws.send('Hello World')}
+    ws.onmessage = e => { 
+      counter.textContent = (e.data == '1') ? e.data + ' visit.' : e.data + ' visits.'
+    }
+  </script>
+  <pre id='counter'>
+`
+
+const sockets = new Set()
+const channel = new BroadcastChannel('')
+
+const kv = await Deno.openKv()
+
+const key = ["counter"]
+
+channel.onmessage = async e => {
+  await kv.atomic().mutate({
+    type: 'sum',
+    key,
+    value: new Deno.KvU64(1n)
+  }).commit()
+
+  const v = await kv.get(key)
+  const counter = v.value.value
+
+  channel.postMessage(counter)
+  sockets.forEach(s => s.send(counter))
+}
+
+Deno.serve((r) => {
+  try {
+    const { socket, response } = Deno.upgradeWebSocket(r)
+    sockets.add(socket)
+    socket.onmessage = channel.onmessage
+    socket.onclose = _ => {
+      sockets.delete(socket)
+      sockets.forEach(s => s.send(sockets.size))
+    }
+    return response
+  } catch {
+    return new Response(html, {headers: {'Content-type': 'text/html'}})
+  }
+})
